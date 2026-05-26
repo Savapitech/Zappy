7777dcc4d86c55a587665e0421f513388fec53f9
titouanhct <titouan.hochet@epitech.eu>
2026-05-26 10:59:19 +0200

    ADD basics defines

---
+++ server/Define.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//Return Value
+#define RVAL_OK 0
+#define RVAL_KO 84
+
+// Ressources densities
+#define FOOD_DENSITY 0.5
+#define LINEMATE_DENSITY 0.3
+#define DERAUMERE_DENSITY 0.15
+#define SIBUR_DENSITY 0.1
+#define MENDIANE_DENSITY 0.1
+#define PHIRAS_DENSITY 0.08
+#define THYSTAME_DENSITY 0.05
