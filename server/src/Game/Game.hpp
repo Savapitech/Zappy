e6887a010448cb4a0c1237970768447dbc704085
titouanhct <titouan.hochet@epitech.eu>
2026-06-01 18:38:48 +0200

    feat(server/game) add new variables inside the game and team header

--- server/src/Game/Game.hpp
+++ server/src/Game/Game.hpp
@@ -10,8 +10,24 @@ namespace game {
   class GameLogic
   {
     private:
-      std::vector<std::unique_ptr<Team>>_teams;
+      std::vector<std::unique_ptr<Team>> _teams;
+      int _mapX;
+      int _mapY;
+      int _freq;
+      int _nbClientMax;
+      int _nextId;
+
     public:
       const std::vector<std::unique_ptr<Team>> &getTeams() const { return _teams; }
+      int getMapX() const { return _mapX; }
+      int getMapY() const { return _mapY; }
+      int getFreq() const { return _freq; }
+      int getClientMax() const { return _nbClientMax; }
+      int getNextId() const { return _nextId; }
+
+      void GameLoop();
+      void lifeUpdate();
+      void ressourcesUpdate();
+      void checkWinCond();
   };
 } //namespace game
