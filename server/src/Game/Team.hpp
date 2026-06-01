93579b89bbd73820c2683544cd07982cf972ef3d
titouanhct <titouan.hochet@epitech.eu>
2026-06-01 18:38:48 +0200

    feat(server/game) add new variables inside the game and team header

--- server/src/Game/Team.hpp
+++ server/src/Game/Team.hpp
@@ -13,7 +13,7 @@ namespace game {
       std::string _name;
       int _nbClientMax;
       int _connected;
-      std::vector<std::unique_ptr<User>> _users;
+      std::vector<std::unique_ptr<Player>> _players;
 
     public:
       Team(const std::string &name, int nbClientMax) : _name(name), _nbClientMax(nbClientMax), _connected(0) {}
@@ -25,12 +25,12 @@ namespace game {
       int getClientMax() const { return _nbClientMax; }
       int getConnected() const { return _connected; }
       int getAvailable() const { return (_nbClientMax - _connected); }
-      const std::vector<std::unique_ptr<User>> &getUsers() const { return _users; }
+      const std::vector<std::unique_ptr<Player>> &getPlayers() const { return _players; }
 
       void addClientMax() { _nbClientMax++; }
       void addConnected() { _connected++; }
       void removeConnected() { _connected--; }
-      void addUser(std::unique_ptr<User> user) { _users.push_back(std::move(user)); }
-      void removeUser(User *user);
+      void addPlayer(std::unique_ptr<Player> user) { _players.push_back(std::move(user)); }
+      void removeUser(Player *user);
   };
 } //namespace game
