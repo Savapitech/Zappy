13e9f7d9fbeb7b8fa5f30e501a74e7cd4e311695
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-06-02 21:44:02 +0200

    feat(gui/graphical pipeline): add the window class and event struct
    Start the window class by adding constructor, destructor and open
    method, the open with X11 and OpenGL
    Add Event Struct, for handling event

---
+++ gui/src/Window/Window.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Event.hpp"
+
+#include <string>
+#include <vector>
+
+namespace Zappy {
+class Window {
+private:
+    void* _display;
+    unsigned long _windowHandle;
+    void* _context;
+    unsigned long _wmDeleteMessage;
+
+    std::vector<Zappy::Event> _events;
+
+public:
+    Window();
+    ~Window();
+
+    void open(unsigned int width, unsigned int height, const std::string& title);
+    //void close();
+    //void swapBuffers();
+
+    //const std::vector<Zappy::Event>& pollEvents();
+};
+}
