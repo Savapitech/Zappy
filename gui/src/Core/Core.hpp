5b4cdcb27b5023bc79ac8947beeaf472e06248f6
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-06-03 00:24:08 +0200

    feat(gui/graphical pipeline): draw sprite into the run method of core class
    - Bind Fragment and Vertex shader to draw one sprite for now

--- gui/src/Core/Core.hpp
+++ gui/src/Core/Core.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "../Window/Window.hpp"
+#include "Texture/Texture.hpp"
+
+#include <memory>
 
 class Core
 {
@@ -10,7 +13,18 @@ class Core
         Zappy::Window _window;
         //GuiManager
         //GameState
+        bool _isRunning;
+
+        std::unique_ptr<Zappy::Texture> _myTexture;
+
+        unsigned int _shaderProgram;
+        unsigned int _VAO;
+        unsigned int _VBO;
+        unsigned int _EBO;
     public:
         Core();
         ~Core();
+
+        void init();
+        void run(void);
 };
