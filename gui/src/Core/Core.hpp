3dc8ead601befd89ff10d2153352d0c19b5f9873
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-06-03 16:21:29 +0200

    fix/feat(gui/graphical pipeline): fix Core by using more class in application with oop encapsulation
    Add Texture Manager: Handle Many Texture for any sprite
    Add Scene Manager - IScene -Menu: Scene Manager who work like a state
    machine, and IScene who describe Scene implementation, like the Menu
    class

--- gui/src/Core/Core.hpp
+++ gui/src/Core/Core.hpp
@@ -3,6 +3,7 @@
 #include "../Window/Window.hpp"
 #include "Texture/Texture.hpp"
 #include "Texture/TextureManager.hpp"
+#include "SceneManager/SceneManager.hpp"
 
 #include "Core/Shader/Shader.hpp"
 #include "Sprite/Sprite.hpp"
@@ -12,27 +13,16 @@
 namespace Zappy {
     class Core
     {
-        private:
-            //Network
-            //ServerParser
-            Zappy::Window _window;
-            //GuiManager
-            //GameState
-            bool _isRunning;
+    private:
+        Zappy::SceneManager _sceneManager;
+        std::unique_ptr<Zappy::Shader> _defaultShader;
+        bool _isRunning;
+        Zappy::Window _window;
+    public:
+        Core();
+        ~Core();
 
-            std::unique_ptr<Zappy::Shader> _defaultShader;
-            TextureManager _textureManager;
-            std::vector<std::unique_ptr<Zappy::Sprite>> _sprites;
-
-            unsigned int _shaderProgram;
-            unsigned int _VAO;
-            unsigned int _VBO;
-            unsigned int _EBO;
-        public:
-            Core();
-            ~Core();
-
-            void init();
-            void run(void);
+        void init();
+        void run(void);
     };
 }
