025d7cfeeb23ed7d117a68e28f3b89104eda4c05
eiffis <enzo.iffis@epitech.eu>
2026-06-06 00:26:08 +0200

    fix(gui/MainTitle): set the font of titleFont bigger and now is centered and same for pressStartFont

--- gui/src/Scene/MainTitle.hpp
+++ gui/src/Scene/MainTitle.hpp
@@ -36,13 +36,14 @@ namespace Zappy {
             MainTitle(TextureManager &tm) : _texManager(tm) {}
             void onEnter() override {
                 _textShader = std::make_unique<Shader>("gui/src/Core/Shader/text.vert", "gui/src/Core/Shader/text.frag");
-                Font &sceneFont = _fontManager.get("gui/assets/fonts/mainTitle.otf");
-                _titleText = std::make_unique<Text>(sceneFont, "ZAPPY", 0.0f, 100.0f);
+                Font &titleFont = _fontManager.get("gui/assets/fonts/mainTitle.otf", 256.0f, 4096);
+                _titleText = std::make_unique<Text>(titleFont, "ZAPPY", 0.0f, 100.0f);
                 _titleText->color = Zappy::Math::vec3(1.0f, 0.8f, 0.0f);
-                _titleText->setScale(2.0f);
                 float titleX = (WIDTH / 2.0f) - (_titleText->getWidth() / 2.0f);
-                _titleText->setPosition(titleX, 100.0f);
-                _pressStartText = std::make_unique<Text>(sceneFont, "Press ANY KEY to start", 0.0f, 200.0f);
+                _titleText->setPosition(titleX, 200.0f);
+
+                Font &startFont = _fontManager.get("gui/assets/fonts/mainTitle.otf", 64.0f, 1024);
+                _pressStartText = std::make_unique<Text>(startFont, "Press ANY KEY to start", 0.0f, 200.0f);
                 _pressStartText->color = Zappy::Math::vec3(1.0f, 1.0f, 1.0f);
                 float pressX = (WIDTH / 2.0f) - (_pressStartText->getWidth() / 2.0f);
                 _pressStartText->setPosition(pressX, 1000.0f);
