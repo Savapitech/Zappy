a94684227ace015be4c273415cce9dfead9210dc
eiffis <enzo.iffis@epitech.eu>
2026-06-06 00:23:59 +0200

    fix(gui/FontManager): add FontSize and atlasSize for the size of the font

--- gui/src/Font/FontManager.hpp
+++ gui/src/Font/FontManager.hpp
@@ -11,13 +11,14 @@ namespace Zappy {
         private:
             std::unordered_map<std::string, std::unique_ptr<Font>> _fonts;
         public:
-            Font &get(const std::string &filepath) {
-            auto it = _fonts.find(filepath);
-            if (it == _fonts.end()) {
-                _fonts[filepath] = std::make_unique<Font>(filepath);
-                return *_fonts[filepath];
+            Font &get(const std::string &filepath, float fontSize = 32.0f, int atlasSize = 512) {
+                std::string key = filepath + "_" + std::to_string(fontSize);
+                auto it = _fonts.find(key);
+                if (it == _fonts.end()) {
+                    _fonts[key] = std::make_unique<Font>(filepath, atlasSize, atlasSize, fontSize);
+                    return *_fonts[key];
+                }
+                return *(it->second);
             }
-            return *(it->second);
-      }
     };
 } // namespace Zappy
