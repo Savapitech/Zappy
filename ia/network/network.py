1a73587ae90d40784e611cd1de6f1b78aaf56991
Gabriel Viel <gabriel.viel@epitech.eu>
2026-06-10 17:03:35 +0200

    feat(network): Asyncio Overhaul the network object to use asyncio instead of nonblocking sockets. It now allows easy nonblocking read and write functions, and a dedicated cleanup function.

--- ia/network/network.py
+++ ia/network/network.py
@@ -1,29 +1,149 @@
-import socket
+import asyncio
+
+# Generator function that waits for data from stream and returns its result after each read.
+# It stops once it reaches EOF.
+async def read_stream(reader: asyncio.StreamReader):
+    while True:
+        # wait for data
+        data = await reader.read(4096)
+        # if eof is reached
+        if not data:
+            return
+        # return read data
+        yield data
+
+# Queue function that allows reading from stream to be nonblocking.
+# Automatically stops with the generator function.
+# It also shuts down the queue to notify processes that depend on it.
+async def bg_reader(reader: asyncio.StreamReader, queue: asyncio.Queue):
+    # Loop on generator function
+    async for data in read_stream(reader):
+        await queue.put(data.decode())
+    # Once loop is broken, shut down the queue
+    queue.shutdown(immediate=True)
+
+# Writer function that takes input from the queue and writes it to the stream.
+# If it recieves empty input, it shuts down the queue and stops.
+async def write_stream(writer: asyncio.StreamWriter, queue: asyncio.Queue):
+    while True:
+        # Wait until data is available in the queue
+        data: str = await queue.get()
+        
+        # If data is empty (simulate eof), shut down the queue and stop
+        if not data:
+            queue.shutdown(immediate=True)
+            return
+        
+        # Write data to stream
+        writer.write(data.encode("utf-8"))
+        
+        # Wait for the buffer to flush (confirm that the write was succesful)
+        await writer.drain()
 
 class network:
-    def __init__(self, host: str, port: int, team: str, machine: str):
+    def __init__(self, port: int, team: str, machine: str):
         self.machine = machine
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.reader = None
+        self.writer = None
+        self.oqueue = asyncio.Queue()
+        self.iqueue = asyncio.Queue()
+        self.reader_task = None
+        self.writer_task = None
         self.team = team
+        self.up = False
 
-    def connect(self):
-        self.socket.connect((self.machine, self.port)) #connect to the machine without handling raise
+    async def connect(self):
+        # Open connection and create stream objects. Can except in case connection fails.
+        self.reader, self.writer = await asyncio.open_connection(self.machine, self.port)
 
-    def disconnect(self):
-        self.socket.close()
+        # Set connection indicator 
+        self.up = True
+
+        # Instantiate reader task
+        self.reader_task = asyncio.create_task(
+            bg_reader(
+                self.reader,
+                self.oqueue
+            )
+        )
+
+        # Instantiate writer task
+        self.writer_task = asyncio.create_task(
+            write_stream(
+                self.writer,
+                self.iqueue
+            )
+        )
+
+    async def disconnect(self):
+        # Close connection
+        self.writer.close()
+
+        # Cancel reader and writer task if they are still running
+        for task in (self.reader_task, self.writer_task):
+            task.cancel()
+            # This must be wrapped in a try block as it raises an exception if it cancelled (even if is its intended behavior)
+            try:
+                await task
+            except asyncio.CancelledError:
+                pass
+
+        # Shutdown both input and output queues
+        self.oqueue.shutdown(immediate=True)
+        self.iqueue.shutdown(immediate=True)
+
+        # Set connection indicator to false
+        self.up = False
 
     def send(self, msg: str):
-        self.socket.send(msg.encode("utf-8"))
+        # Do nothing in case connection is closed
+        if not self.up:
+            return
+        try:
+            # Add element to queue
+            self.iqueue.put_nowait(msg)
+
+        # In case of something happening on the writer task, queue is shut down.
+        # This sets the connection indicator to false, and will prevent further I/O operations.
+        except asyncio.QueueShutDown:
+            self.up = False
 
     def read(self):
-        buffer = self.socket.recv(1024)
-        return buffer.decode("utf-8") #return the all the msg sended by the server
+        # Do nothing in case connection is closed
+        if not self.up:
+            return
+        
+        # Try to read from queue
+        try:
+            # Try to get one element from queue
+            return self.oqueue.get_nowait()
+        
+        # In case of something happening on the reader task, queue is shut down.
+        # This sets the connection indicator to false, and will prevent further I/O operations.
+        except asyncio.QueueShutDown:
+            self.up = False
 
-def connect(port: int, team: str, machine: str):
+        # In case no elements are present in the queue.
+        # It is done that way to ensure that if the stream is empty, the QueueShutdown state can be detected
+        except asyncio.QueueEmpty:
+            pass
+
+        # If no elements are present in the queue (or queue is shutdown), fallback to empty str
+        return ""
+
+
+
+async def connect(port: int, team: str, machine: str):
+    # Instantiate object
     connection = network(port, team, machine)
+    # Wrap the connection in a try block as it can except
     try:
-        connection.connect()
-    except:
+        # Wait for connection
+        await connection.connect()
+    except Exception:
+        # In case anything happens, the client cannot be launched (dependant on connection), and it raises an error
         raise(Exception("Connection to the server failed."))
+    
+    # Return connection object after it is initialised
     return connection
