8d9baaee272b97168b9c2069e720f0c391a21228
eiffis <enzo.iffis@epitech.eu>
2026-06-10 22:30:57 +0200

    fix(gui/audio): fix audioManager now is exactly the same as textureManager and refactor the audio class. the audio object have now his filepath in private var

--- gui/src/Audio/audioManager.hpp
+++ gui/src/Audio/audioManager.hpp
@@ -2,17 +2,22 @@
 
 #include <memory>
 #include <string>
-
+#include <unordered_map>
 #include "audio.hpp"
 
 namespace Zappy {
     class audioManager {
         private:
-            Audio _music;
+            std::unordered_map<std::string, std::unique_ptr<Audio>> _audios;
         public:
-            audioManager(const std::string &filepath){
-                _music.playMusic(filepath);
+            Audio &get(const std::string &filepath) {
+                auto it = _audios.find(filepath);
+
+                if (it == _audios.end()) {
+                    _audios[filepath] = std::make_unique<Audio>(filepath);
+                    return *_audios[filepath];
+                }
+            return *(it->second);
             }
-            void stopMusic() {_music.stopMusic();}
     };
 }
