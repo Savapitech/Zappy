315a6b7f5d3e509e4607b271f7ef7db061335044
eiffis <enzo.iffis@epitech.eu>
2026-06-12 01:02:48 +0200

    feat(gui/quickMenu): add ui shaders .vert and .frag for the ui in 2D for the quickMenu

--- gui/src/Scene/quickMenu.hpp
+++ gui/src/Scene/quickMenu.hpp
@@ -16,27 +16,33 @@ namespace Zappy {
             TextureManager &_texManager;
             std::unique_ptr<Sprite> _backgroundSprite;
             std::vector<std::unique_ptr<Sprite>> _buttons;
+            std::unique_ptr<Shader> _uiShader;
         public:
             quickMenu(TextureManager &tm) : _texManager(tm) {}
             void onEnter() override {
                 Texture& menuTex = _texManager.get("gui/assets/quickMenu.png");
+                _uiShader = std::make_unique<Shader>("gui/src/Core/Shader/ui.vert", "gui/src/Core/Shader/ui.frag");
                 _backgroundSprite = std::make_unique<Sprite>(menuTex);
-                _backgroundSprite->setPosition(960.0f, 900.0f);
-                _backgroundSprite->scale = Zappy::Math::vec3(540.0f, 540.0f, 1.0f);
+                _backgroundSprite->setPosition(960.0f, 332.5f);
+                _backgroundSprite->scale = Zappy::Math::vec3(415.0f, 415.0f, 1.0f);
             }
             SceneState update(const std::vector<Zappy::Event> &events, const Zappy::GameState &gameState, const std::vector<Zappy::NetworkEvent> &netEvents, float deltaTime) override 
             {
                 return SceneState::NONE;
             }
             void draw(Shader &shader) override {
-                if (!_backgroundSprite)
+                if (!_backgroundSprite || !_uiShader)
                     return;
+                glDisable(GL_DEPTH_TEST);
+                _uiShader->bind();
                 Zappy::Math::mat4 orthoProjection = Zappy::Math::ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f);
                 Zappy::Math::mat4 view;
-                _backgroundSprite->draw(shader, view, orthoProjection);
+                _backgroundSprite->draw(*_uiShader, view, orthoProjection);
+                glEnable(GL_DEPTH_TEST);
             }
             void onExit() override {
                 _backgroundSprite.reset();
+                _uiShader.reset();
             }
     };
 }
