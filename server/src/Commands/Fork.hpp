fdfff6ee2ee28c31b373f29bf66db9fec86220fe
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-06-17 15:41:21 +0200

    feat(server): queue ai commands and respect their per-action time cost

--- server/src/Commands/Fork.hpp
+++ server/src/Commands/Fork.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "ACommand.hpp"
+#include "Game/Common.hpp"
 
 namespace commands {
 
 class Fork : public ACommand {
   void execute(std::shared_ptr<Client> client,
                std::vector<std::string> &args) override;
+  int getCost() const override { return TIME_FORK; }
 };
 
 } // namespace commands
--- server/src/Commands/Right.hpp
+++ server/src/Commands/Right.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "ACommand.hpp"
+#include "Game/Common.hpp"
 
 namespace commands {
 
 class Right : public ACommand {
   void execute(std::shared_ptr<Client> client,
                std::vector<std::string> &args) override;
+  int getCost() const override { return TIME_RIGHT; }
 };
 
 } // namespace commands
