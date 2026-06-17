bb7e9775a348532ae7db81e673f371ad0903f4d6
eiffis <enzo.iffis@epitech.eu>
2026-06-17 15:56:16 +0200

    feat(gui/Buttons): add button interface and button class for implementation of buttons

---
+++ gui/src/Buttons/IButton.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Core/Shader/Shader.hpp"
+#include "Event.hpp"
+#include <vector>
+
+namespace Zappy 
+{
+    class IButton {
+        private:
+        public:
+            virtual ~IButton() = default;
+            virtual void draw(Shader &shader) = 0;
+            virtual void setPosition(float x, float y) = 0;
+            virtual void update(const std::vector<Zappy::Event> &events) = 0;
+    };
+}
--- gui/src/Scene/QuickMenu.hpp
+++ gui/src/Scene/QuickMenu.hpp
@@ -1,8 +1,10 @@
 #pragma once
+
 #include "IScene/IScene.hpp"
 #include "Texture/TextureManager.hpp"
 #include "Sprite/Sprite.hpp"
 #include "Logger.hpp"
+#include "Buttons/Button.hpp"
 #include "Render/Render.hpp"
 #include <memory>
 #include <vector>
@@ -15,19 +17,28 @@ namespace Zappy {
         private:
             TextureManager &_texManager;
             std::unique_ptr<Sprite> _backgroundSprite;
-            std::vector<std::unique_ptr<Sprite>> _buttons;
+            std::vector<std::unique_ptr<Zappy::IButton>> _buttons;
             std::unique_ptr<Shader> _uiShader;
         public:
             quickMenu(TextureManager &tm) : _texManager(tm) {}
             void onEnter() override {
                 Texture& menuTex = _texManager.get("gui/assets/quickMenu.png");
+                Texture& buttonTex = _texManager.get("gui/assets/speedButton.png");
                 _uiShader = std::make_unique<Shader>("gui/src/Core/Shader/ui.vert", "gui/src/Core/Shader/ui.frag");
                 _backgroundSprite = std::make_unique<Sprite>(menuTex);
                 _backgroundSprite->setPosition(960.0f, 332.5f);
+                auto myAction = []() {
+                    std::cout << "Bouton cliqué !" << std::endl;
+                    throw;
+                };
+                _buttons.push_back(std::make_unique<Zappy::Button>(buttonTex, 960.0f, 400.0f, 100.0f, 100.0f, myAction));
                 _backgroundSprite->scale = Zappy::Math::vec3(415.0f, 415.0f, 1.0f);
             }
             SceneState update(const std::vector<Zappy::Event> &events, const Zappy::GameState &gameState, const std::vector<Zappy::NetworkEvent> &netEvents, float deltaTime) override 
             {
+                for (auto& button : _buttons) {
+                    button->update(events);
+                }
                 return SceneState::NONE;
             }
             void draw(Shader &shader) override {
@@ -38,9 +49,13 @@ namespace Zappy {
                 Zappy::Math::mat4 orthoProjection = Zappy::Math::ortho(0.0f, WIDTH, HEIGHT, 0.0f, -1.0f, 1.0f);
                 Zappy::Math::mat4 view;
                 _backgroundSprite->draw(*_uiShader, view, orthoProjection);
+                for (auto &button : _buttons) {
+                    button->draw(*_uiShader);
+                }
                 glEnable(GL_DEPTH_TEST);
             }
             void onExit() override {
+                _buttons.clear();
                 _backgroundSprite.reset();
                 _uiShader.reset();
             }
