8f597862f8522f8f75f19f82209a348b96b20019
eiffis <enzo.iffis@epitech.eu>
2026-06-18 16:22:40 +0200

    feat(gui/quickMenu): add buttons for game speed in QM

--- gui/src/Scene/QuickMenu.hpp
+++ gui/src/Scene/QuickMenu.hpp
@@ -5,6 +5,7 @@
 #include "Sprite/Sprite.hpp"
 #include "Logger.hpp"
 #include "Buttons/Button.hpp"
+#include"Network/TcpClient.hpp"
 #include "Render/Render.hpp"
 #include <memory>
 #include <vector>
@@ -17,20 +18,31 @@ namespace Zappy {
         private:
             TextureManager &_texManager;
             std::unique_ptr<Sprite> _backgroundSprite;
+            std::unique_ptr<INetworkClient> _client;
             std::vector<std::unique_ptr<Zappy::IButton>> _buttons;
             std::unique_ptr<Shader> _uiShader;
         public:
             quickMenu(TextureManager &tm) : _texManager(tm) {}
             void onEnter() override {
                 Texture& menuTex = _texManager.get("gui/assets/quickMenu.png");
-                Texture& buttonTex = _texManager.get("gui/assets/speedButton.png");
+                Texture& speedButtonTex = _texManager.get("gui/assets/speedButton.png");
+                Texture& decreaseButtonTex = _texManager.get("gui/assets/minus.png");
+                Texture& increaseButtonTex = _texManager.get("gui/assets/plus.png");
                 _uiShader = std::make_unique<Shader>("gui/src/Core/Shader/ui.vert", "gui/src/Core/Shader/ui.frag");
                 _backgroundSprite = std::make_unique<Sprite>(menuTex);
                 _backgroundSprite->setPosition(960.0f, 332.5f);
-                auto myAction = []() {
-                    std::cout << "Bouton cliqué !" << std::endl;
+                auto helper = []() {
+                    std::cout << "You can decrease / increase the number of ticks per seconds." << std::endl;
                 };
-                _buttons.push_back(std::make_unique<Zappy::Button>(buttonTex, 950.0f, 450.0f, 80.0f, 80.0f, myAction));
+                auto decrease = []() {
+                    std::cout << "decrease the number of ticks per seconds." << std::endl;
+                };
+                auto increase = []() {
+                    std::cout << "increase the number of ticks per seconds." << std::endl;
+                };
+                _buttons.push_back(std::make_unique<Zappy::Button>(speedButtonTex, 950.0f, 450.0f, 80.0f, 80.0f, helper));
+                _buttons.push_back(std::make_unique<Zappy::Button>(increaseButtonTex, 1050.0f, 465.0f, 50.0f, 50.0f, increase));
+                _buttons.push_back(std::make_unique<Zappy::Button>(decreaseButtonTex, 850.0f, 465.0f, 50.0f, 50.0f, decrease));
                 _backgroundSprite->scale = Zappy::Math::vec3(415.0f, 415.0f, 1.0f);
             }
             SceneState update(const std::vector<Zappy::Event> &events, const Zappy::GameState &gameState, const std::vector<Zappy::NetworkEvent> &netEvents, float deltaTime) override 
