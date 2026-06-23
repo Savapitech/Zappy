3cbdcb1cbf2852de3e28236a98b847818fd878ca
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-06-23 23:29:17 +0200

    feat(argument): add argument to the main

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,14 +1,34 @@
 #include "Core/Core.hpp"
 #include "Logger.hpp"
 
-int main(void) {
+int main(int ac, char **av) {
   Logger::setLevel(L_DEBUG);
   LOG_DEBUG("Start gui");
 
   try {
     Zappy::Core core;
 
-    core.init("127.0.0.1", 4242);
+    std::string ip;
+    std::string machine;
+
+    if (ac == 5){
+      if (std::string(av[1]) == "-p")
+        ip = av[2];
+      if (std::string(av[3]) == "-h")
+        machine = av[4];
+    } else {
+        throw std::runtime_error("USAGE: ./zappy_gui -p port -h machine");
+    }
+
+    try
+    {
+      core.init(ip, std::stoi(machine));
+    }
+    catch(const std::exception& e)
+    {
+      throw std::runtime_error("USAGE: ./zappy_gui -p port -h machine");
+    }
+    
     core.run();
 
   } catch (const std::exception &e) {
